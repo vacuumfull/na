10a6a0a1704829c8ef81ebb60b9011b7c6ad89b0
Kosenkov P. V <kosenkov_pv@kodeks.ru>
2017-10-10 16:02:16 +0300

    Places add list and details

--- place/views.py
+++ place/views.py
@@ -1,7 +1,15 @@
 """Places with locations view."""
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 from place.models import Place
+
+
+class PlaceList(ListView):
+    """Place with locations list"""
+
+    model = Place
+    context_object_name = 'places'
 
 
 class PlaceView(DetailView):
