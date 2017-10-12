3acf06ce19c6ad508e70001cec76c4f6662cba45
Kosenkov P. V <kosenkov_pv@kodeks.ru>
2017-10-12 11:04:43 +0300

    BLog -> add last_blog method

--- under/views.py
+++ under/views.py
@@ -10,7 +10,7 @@ class IndexView(TemplateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context['blogs'] = Blog.objects.all()[:4]
+        context['blogs'] = Blog.objects.last_blog()
         context['events'] = Event.objects.all()[:4]
         context['places'] = Place.objects.all()[:4]
         return context
