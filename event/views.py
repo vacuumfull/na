027a625e6fa7c98c073f3ddf46fd60c6ca5e4ce3
Kosenkov P. V <kosenkov_pv@kodeks.ru>
2017-10-18 15:25:50 +0300

    Event -> creat and update form

--- event/views.py
+++ event/views.py
@@ -1,6 +1,8 @@
 """Event view."""
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView
 
 from event.models import Event
 
@@ -16,3 +18,27 @@ class EventView(DetailView):
     """Event details view."""
 
     model = Event
+
+
+class EventCreate(CreateView):
+    """Create blog post."""
+
+    model = Event
+    fields = ['title', 'image', 'description', 'date', 'price',
+              'bands', 'musicians', 'locations']
+    success_url = reverse_lazy('event:index')
+
+    def form_valid(self, form):
+        """Add user info to form."""
+        instance = form.save(commit=False)
+        instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class EventUpdate(UpdateView):
+    """Update blog post."""
+
+    model = Event
+    fields = ['title', 'image', 'description', 'date', 'price',
+              'bands', 'musicians', 'locations']
+    success_url = reverse_lazy('event:index')
