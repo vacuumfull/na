425abf38ad2290932edab09673ef8c6b75697943
Kosenkov P. V <kosenkov_pv@kodeks.ru>
2017-10-19 11:11:45 +0300

    Band -> add create and update actions

--- band/urls.py
+++ band/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 
-from band.views import BandList, BandDetail
+from band.views import BandList, BandCreate, BandDetail, BandUpdate
 
 
 app_name = 'band'
 urlpatterns = [
-    url(r'(?P<slug>[-\w]+)/$', BandDetail.as_view(), name='view'),
     url(r'^$', BandList.as_view(), name='index'),
+    url(r'^new/$', BandCreate.as_view(), name='create'),
+    url(r'^(?P<slug>[-\w]+)/$', BandDetail.as_view(), name='view'),
+    url(r'^(?P<slug>[-\w]+)/edit/$', BandUpdate.as_view(), name='edit'),
 ]
