a6e27127a4ae81ad6d10cbe32ab82630389a4418
Kosenkov P. V <kosenkov_pv@kodeks.ru>
2017-11-01 18:02:43 +0300

    Blog -> update api model

--- blog/api.py
+++ blog/api.py
@@ -1,10 +1,10 @@
 """Blog api."""
-from blog.models import Blog, Comment as BlogComment, Rating as BlogRating
+from blog.models import Blog, Comment, Rating
 
 
 def blog_rating(blog_id: int, user: object) -> dict:
     """Get blog rating."""
-    result = BlogRating.objects.average(blog_id, user)
+    result = Rating.objects.average(blog_id, user)
     return result
 
 
@@ -12,7 +12,7 @@ def vote_blog_rating(blog_id: int, user: object, vote: int) -> dict:
     """Vote for blog post."""
     try:
         blog = Blog.objects.get(pk=blog_id)
-        BlogRating.objects.update_or_create(
+        Rating.objects.update_or_create(
             blog=blog, user=user,
             defaults={'value': vote})
     except Blog.DoesNotExist:
@@ -21,5 +21,5 @@ def vote_blog_rating(blog_id: int, user: object, vote: int) -> dict:
 
 def blog_comment(blog_id: int, offset: int) -> dict:
     """Get blog rating."""
-    result = BlogComment.objects.get_last_comments(blog_id, int(offset))
+    result = Comment.objects.get_last_comments(blog_id, int(offset))
     return result
