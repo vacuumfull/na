bae34ad084bbf9a57724aafb0af35010234206b3
Ildar Khairullin <mr.willentretenmert@gmail.com>
2017-11-20 17:11:04 +0200

    fix

--- under/settings.py
+++ under/settings.py
@@ -143,7 +143,6 @@ STATICFILES_DIRS = [
     ('js', 'static/js'),
     ('images', 'static/images'),
     ('dist', 'static/dist'),
-    ('datepicker', 'static/datepicker'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'upload')
