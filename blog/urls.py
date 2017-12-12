74b80da49f70dd8af72020cb6d770e8d06ae8227
Kosenkov P. V <quild@mail.ru>
2017-12-12 17:26:56 +0300

    [change] Replace url to path in urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,14 @@
-from django.conf.urls import url
+from django.urls import path, re_path
 
 from blog.views import (IndexList, BlogCreate, BlogView, BlogUpdate)
 
+
 app_name = 'blog'
 urlpatterns = [
-    url(r'^$', IndexList.as_view(), name='index'),
-    url(r'^_rubrics/(?P<rubric>(imho|festivals|music))/$',
-        IndexList.as_view(), name='rubric'),
-    url(r'^new/$', BlogCreate.as_view(), name='create'),
-    url(r'^(?P<slug>[-\w]+)/$', BlogView.as_view(), name='view'),
-    url(r'^(?P<slug>[-\w]+)/edit/$', BlogUpdate.as_view(), name='edit'),
+    path('', IndexList.as_view(), name='index'),
+    re_path(r'^_rubrics/(?P<rubric>(imho|festivals|music))/$',
+            IndexList.as_view(), name='rubric'),
+    path('new/', BlogCreate.as_view(), name='create'),
+    path('<slug:slug>/', BlogView.as_view(), name='view'),
+    path('<slug:slug>/edit/', BlogUpdate.as_view(), name='edit'),
 ]
--- place/urls.py
+++ place/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url
+from django.urls import path
 
 from place.views import PlaceCreate, PlaceList, PlaceUpdate, PlaceView, MapView
 
 
 app_name = 'place'
 urlpatterns = [
-    url(r'^$', PlaceList.as_view(), name='index'),
-    url(r'^map/$', MapView.as_view(), name='map'),
-    url(r'^new/$', PlaceCreate.as_view(), name='create'),
-    url(r'^(?P<slug>[-\w]+)/$', PlaceView.as_view(), name='view'),
-    url(r'^(?P<slug>[-\w]+)/edit/$', PlaceUpdate.as_view(), name='edit'),
+    path('', PlaceList.as_view(), name='index'),
+    path('map/', MapView.as_view(), name='map'),
+    path('new/', PlaceCreate.as_view(), name='create'),
+    path('<slug:slug>/', PlaceView.as_view(), name='view'),
+    path('<slug:slug>/edit/', PlaceUpdate.as_view(), name='edit'),
 ]
