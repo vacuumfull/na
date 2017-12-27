4e626d48ebc6794e8946996a756a5cfb13f6daa5
Kosenkov P. V <quild@mail.ru>
2017-12-27 12:28:27 +0300

    Add tags app

---
+++ tag/models.py
@@ -0,0 +1,11 @@
+"""Tags model."""
+from django.db import models
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=60)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
