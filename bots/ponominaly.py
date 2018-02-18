ba07fc85fee7eefb22f0a378abe3f8055612051c
Your Name <mr.willentretenmert@gmail.com>
2018-02-18 20:09:29 +0300

    Fixed duplicate parsing error

--- bots/ponominaly.py
+++ bots/ponominaly.py
@@ -5,57 +5,57 @@ from grab.spider import Task, Spider
 from event.tasks import check_with_cache
 
 class PonaminaluSpider(Spider):
-    # Обязательный аргумент стартовых URL
-    # limit = 1000 - ибо, что нас ограничивает забирать всё? ;)
-    base_url = "https://spb.ponominalu.ru"
-    initial_urls = [base_url + "/ajax/category/concerts?sort_by=date&sort_direction=desc&limit=1000"]
-    locations = ['Гештальт', 'Космонавт', 'Зал ожидания', 'Aurora']
-    monthes = [{'Янв': '01'}, {'Фев': '02'}, {'Мар': '03'}, {'Апр': '04'},
-            {'Май': '05'}, {'Июн': '06'}, {'Июл': '07'}, {'Авг': '08'},
-            {'Сент': '09'}, {'Окт': '10'}, {'Нояб': '11'}, {'Дек': '12'}]
+	# Обязательный аргумент стартовых URL
+	# limit = 1000 - ибо, что нас ограничивает забирать всё? ;)
+	base_url = "https://spb.ponominalu.ru"
+	initial_urls = [base_url + "/ajax/category/concerts?sort_by=date&sort_direction=desc&limit=1000"]
+	locations = ['Гештальт', 'Космонавт', 'Зал ожидания', 'Aurora']
+	monthes = [{'Янв': '01'}, {'Фев': '02'}, {'Мар': '03'}, {'Апр': '04'},
+			{'Мая': '05'},
+			{'Май': '05'}, {'Июн': '06'}, {'Июл': '07'}, {'Авг': '08'},
+			{'Сент': '09'}, {'Окт': '10'}, {'Нояб': '11'}, {'Дек': '12'}]
 
-    def task_initial(self, grab, task):
-        """
-        Обязательная первая задача
-        Ищем ссылки на подразделы
-        """
-        print('Load main page')
-        for elem in grab.doc.select('//div[@class="events-list"]/div[@class="events-list__item event"]'):
-            info_url = self.base_url + elem.select('a/@href').text()
-            info = {}
-            info['image'] = 'http://' + elem.select('a/img/@src').text().lstrip('/')
-            info['title'] = elem.select(
-                'article[@class="event__info bs-bx"]/div[@class="event__title"]/a/span').text()
-            info['date'] = elem.select(
-                'article[@class="event__info bs-bx"]/time[@class="event__time"]').text()
-            info['location'] = elem.select(
-                'article[@class="event__info bs-bx"]/div[@class="event__venue"]/a/span').text()
-            info['description'] = info_url
-            for item in self.locations:
-                if info['location'].find(item) > -1:
-                    yield Task('load_info', url=info_url, info=copy.deepcopy(info))
+	def task_initial(self, grab, task):
+		"""
+		Обязательная первая задача
+		Ищем ссылки на подразделы
+		"""
+		print('Load main page')
+		for elem in grab.doc.select('//div[@class="events-list"]/div[@class="events-list__item event"]'):
+			info_url = self.base_url + elem.select('a/@href').text()
+			info = {}
+			info['image'] = 'http://' + elem.select('a/img/@src').text().lstrip('/')
+			info['title'] = elem.select(
+				'article[@class="event__info bs-bx"]/div[@class="event__title"]/a/span').text()
+			info['date'] = elem.select(
+				'article[@class="event__info bs-bx"]/time[@class="event__time"]').text()
+			info['location'] = elem.select(
+				'article[@class="event__info bs-bx"]/div[@class="event__venue"]/a/span').text()
+			info['description'] = info_url
+			for item in self.locations:
+				if info['location'].find(item) > -1:
+					yield Task('load_info', url=info_url, info=copy.deepcopy(info))
 
 
-    def task_load_info(self, grab, task, **kwargs):
-        """ Обработка собственно карточки """
-        # Тут большая и жирная логика, чего надо збарть уже со страницы, типа аннотаций и рпочего
-        # Так как сайт являеться частичным агрегатором, тот тут надо писать условия разные
-        print(task.url, task.info)
-        shorted = task.info.get('date')[:-6]
-        splited = shorted.split(' ')
-        day = str(splited[0]) if int(splited[0]) > 9 else '0' + str(splited[0])
-        month = ""
-        for mon in self.monthes:
-            for item in mon.keys():
-                if splited[1].find(item.lower()) > -1:
-                    month = mon[item]
-        date = str(day) + str(month) + str(splited[2])
-        print(date)
-        task.info['date'] = datetime.datetime.strptime(date, '%d%m%Y').date()
-        check_with_cache.delay(task.info)
+	def task_load_info(self, grab, task, **kwargs):
+		""" Обработка собственно карточки """
+		# Тут большая и жирная логика, чего надо збарть уже со страницы, типа аннотаций и рпочего
+		# Так как сайт являеться частичным агрегатором, тот тут надо писать условия разные
+		print(task.url, task.info)
+		shorted = task.info.get('date')[:-6]
+		splited = shorted.split(' ')
+		day = str(splited[0]) if int(splited[0]) > 9 else '0' + str(splited[0])
+		month = ""
+		for mon in self.monthes:
+			for item in mon.keys():
+				if splited[1].find(item.lower()) > -1:
+					month = mon[item]
+		date = str(day) + str(month) + str(splited[2])
+		task.info['date'] = datetime.datetime.strptime(date, '%d%m%Y').date().isoformat()
+		check_with_cache.delay(task.info)
 
 
 if __name__ == "__main__":
-    # Создание бота на основе нашего Паука
-    BOT = PonaminaluSpider()
-    BOT.run()
+	# Создание бота на основе нашего Паука
+	BOT = PonaminaluSpider()
+	BOT.run()
--- event/tasks.py
+++ event/tasks.py
@@ -6,80 +6,95 @@ from urllib.request import urlretrieve
 from event.models import Event
 from under.celery import app
 from django.core.cache import cache
+from datetime import timedelta
 
 
 @app.task(name='event.tasks.events_to_cache')
 def events_to_cache():
-    events = Event.objects.values('id', 'title', 'date')
-    list_result = [i for i in events]
-    cache.set('event_titles', list_result, 3600*24)
+	events = Event.objects.values('title')
+	list_result = [i for i in events]
+	#for item in list_result:
+	#	item['date'] = item['date'].strftime('%Y-%m-%d')
+	cache.set('event_titles', list_result, 3600*24)
 
 
 @app.task(name='event.tasks.check_with_cache')
 def check_with_cache(info):
-    events = cache.get('event_titles')
-    # На проде этот блок надо выпилить, events_to_cache 
-    # должен раз в день утром обновлять кэш
-    if isinstance(events, type(None)):
-        events_to_cache()
-        time.sleep(2)
-        events = cache.get('event_titles')
-        check_or_update(events, info)
-    else:
-       check_or_update(events, info)
+	events = cache.get('event_titles')
+	# На проде этот блок надо выпилить, events_to_cache 
+	# должен раз в день утром обновлять кэш
+	if isinstance(events, type(None)):
+		events_to_cache()
+		time.sleep(2)
+		events = cache.get('event_titles')
+		check_or_update(events, info)
+	else:
+	   check_or_update(events, info)
 
 
 def check_or_update(events, info):
-    count = 0
-    for item in events:
-        if compare(item, info) == 0:
-            count += 1
-    if count > 0:
-        info['image']= upload_image(info['image'])
-        if not info['title']:
-            info['title'] = 'какое-то событие' 
-        
-        Event.objects.create(title=info['title'], description=info['description'], image=info['image'][7:], date=info['date'], owner_id=1, published=False)
-        update_cache(events, info)     
+	isFound = False
+	for item in events:
+		if compare(item, prepare_info(info)) > 0:
+			isFound = True
+	if isFound is False:
+		info['image'] = upload_image(info['image'])
+		if not info['title']:
+			info['title'] = 'какое-то событие' 
+		
+		Event.objects.create(title=info['title'], description=info['description'], image=info['image'][7:], date=info['date'], owner_id=1, published=False)
+		update_cache(info)
 
 
 def compare(dict1, dict2):
-    diff = set(dict1.items()) & set(dict2.items())
-    return len(diff)
+	diff = set(dict1.items()) & set(dict2.items())
+	return len(diff)
+
+def prepare_info(info_dict):
+	copy_dict = info_dict.copy()
+	copy_dict.pop('date', None)
+	copy_dict.pop('image', None)
+	copy_dict.pop('location', None)
+	copy_dict.pop('description', None)
+	return copy_dict
 
 
-def update_cache(events, new_dict):
-    events.append(new_dict)
-    cache.set('event_titles', events, 3600*24)
+def update_cache(new_dict):
+	events = cache.get('event_titles')
+	events.append(prepare_info(new_dict))
+	cache.set('event_titles', events, 3600*24)
 
 
 def format_img_path(img_path):
-    """Format image path."""
-    img_info = {}
-    splited = img_path.split(".")
-    #get extension
-    ext = splited[len(splited)-1]
-    name = str(base64.b64encode(img_path.encode('utf-8')))
-    img_info['ext'] = ext
-    # срезаем лишние символы
-    img_info['name'] = name[3:-4]
-    return img_info
+	"""Format image path."""
+	img_info = {}
+	splited = img_path.split(".")
+	#get extension
+	ext = splited[len(splited)-1]
+	name = str(base64.b64encode(img_path.encode('utf-8')))
+	img_info['ext'] = ext
+	# срезаем лишние символы
+	if len(name) > 40:
+		img_info['name'] = name[30:-3]
+	else:
+		img_info['name'] = name[3:-3]
+	
+	return img_info
 
 
 def upload_image(img_path):
-    """Upload image to dir."""
-    upload_dir = "upload/parser_images/"
-    img_info = format_img_path(img_path)
-    target_path = (upload_dir +
-                    "_" +
-                    img_info.get('name') +
-                    '.' +
-                    img_info.get('ext'))
-    try:
-        urlretrieve(img_path, target_path)
-        return target_path
-    except FileNotFoundError as err:
-        print(err)   # something wrong with local path
-    except HTTPError as err:
-        print(err)  # something wrong with url
-
+	"""Upload image to dir."""
+	upload_dir = "upload/parser_images/"
+	img_info = format_img_path(img_path)
+	target_path = (upload_dir +
+					"_" +
+					img_info.get('name') +
+					'.' +
+					img_info.get('ext'))
+	try:
+		urlretrieve(img_path, target_path)
+		return target_path
+	except FileNotFoundError as err:
+		print(err)   # something wrong with local path
+	except HTTPError as err:
+		print(err)  # something wrong with url
