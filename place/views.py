68a4bbeb16460c78e4ed917908a8fe4c96831f13
Your Name <mr.willentretenmert@gmail.com>
2018-02-24 19:10:58 +0300

    Added current events in places

--- place/views.py
+++ place/views.py
@@ -1,6 +1,6 @@
 """Places with locations view."""
 from django.contrib.auth.mixins import (LoginRequiredMixin,
-                                        PermissionRequiredMixin)
+										PermissionRequiredMixin)
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
@@ -17,83 +17,89 @@ from member.models import UserExtend
 
 
 class PlaceList(ListView):
-    """Place with locations list"""
+	"""Place with locations list"""
 
-    queryset = Place.objects.published()
-    context_object_name = 'places'
-    paginate_by = 16
+	queryset = Place.objects.published()
+	context_object_name = 'places'
+	paginate_by = 16
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        if self.request.user.is_authenticated:
-            styles = UserExtend.objects.filter(user=self.request.user).values('prefer_styles')
-            if styles[0]['prefer_styles'] is not None:
-                context['prefered'] = Place.objects.filter(published=True, tags__name__in=json.loads(styles[0]['prefer_styles'])).distinct()
-        return context
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		if self.request.user.is_authenticated:
+			styles = UserExtend.objects.filter(user=self.request.user).values('prefer_styles')
+			if styles[0]['prefer_styles'] is not None:
+				context['prefered'] = Place.objects.filter(published=True, tags__name__in=json.loads(styles[0]['prefer_styles'])).distinct()
+		return context
 
 
 class PlaceView(DetailView):
-    """Detail view places."""
+	"""Detail view places."""
 
-    model = Place
+	model = Place
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+
+		context['events'] = Event.objects.filter(locations=self.get_object().pk, published=True)
+		return context
 
 
 
 class PlacesUserView(LoginRequiredMixin, TemplateView):
-    """User places list"""
+	"""User places list"""
 
-    template_name = 'place/place_user_list.html'
+	template_name = 'place/place_user_list.html'
 
 
 class PlaceCreate(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
-    """Create blog post."""
+	"""Create blog post."""
 
-    permission_required = 'place.add_place'
-    form_class = PlaceModelForm
-    model = Place
-    success_url = reverse_lazy('place:list')
+	permission_required = 'place.add_place'
+	form_class = PlaceModelForm
+	model = Place
+	success_url = reverse_lazy('place:list')
 
-    def form_valid(self, form):
-        """Add user info to form."""
-        instance = form.save(commit=False)
-        instance.owner = self.request.user
-        instance.save()
-        form.save_m2m()
-        return super().form_valid(form)
+	def form_valid(self, form):
+		"""Add user info to form."""
+		instance = form.save(commit=False)
+		instance.owner = self.request.user
+		instance.save()
+		form.save_m2m()
+		return super().form_valid(form)
 
 
 class PlaceUpdate(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
-    """Update blog post."""
+	"""Update blog post."""
 
-    permission_required = 'place.change_place'
-    form_class = PlaceModelForm
-    model = Place
-    success_url = reverse_lazy('place:list')
-    template_name = 'place/place_update.html'
+	permission_required = 'place.change_place'
+	form_class = PlaceModelForm
+	model = Place
+	success_url = reverse_lazy('place:list')
+	template_name = 'place/place_update.html'
 
-    def get_object(self, queryset=None):
-        """Return 404 if user not owner object."""
-        return get_object_or_404(
-            self.model, slug=self.kwargs["slug"], owner=self.request.user)
+	def get_object(self, queryset=None):
+		"""Return 404 if user not owner object."""
+		return get_object_or_404(
+			self.model, slug=self.kwargs["slug"], owner=self.request.user)
 
-    def form_valid(self, form):
-        """Add user info to form."""
-        instance = form.save(commit=False)
-        instance.owner = self.request.user
-        instance.save()
-        form.save_m2m()
-        return super().form_valid(form)
+	def form_valid(self, form):
+		"""Add user info to form."""
+		instance = form.save(commit=False)
+		instance.owner = self.request.user
+		instance.save()
+		form.save_m2m()
+		return super().form_valid(form)
 
 
 class PlaceDelete(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
-    """Delete place post."""
+	"""Delete place post."""
 
-    permission_required = 'place.delete_place'
-    model = Place
-    success_url = reverse_lazy('place:index')
+	permission_required = 'place.delete_place'
+	model = Place
+	success_url = reverse_lazy('place:index')
 
 
 class MapView(TemplateView):
-    """Maps page"""
+	"""Maps page"""
 
-    template_name = 'place/place_map.html'
+	template_name = 'place/place_map.html'
