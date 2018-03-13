cc976a64d91e8a255c42347ca89bc241b005c279
Your Name <mr.willentretenmert@gmail.com>
2018-03-13 23:39:29 +0300

    Added help messages and updated design

--- member/urls.py
+++ member/urls.py
@@ -3,7 +3,6 @@ from django.contrib.auth.views import LoginView, logout
 
 from member.views import SignupUser, SettingsView, UserView
 
-
 urlpatterns = [
     path('settings/', SettingsView.as_view(), name='settings'),
     path('login/', LoginView.as_view(), name='login'),
