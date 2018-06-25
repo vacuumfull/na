67ef8219b4c49dd3962509bd481a1c1b8aafc6a3
Ildar Khairullin <mr.willentretenmert@gmail.com>
2018-06-25 14:35:48 +0200

    Removed settings and oauth

--- oauth/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class Oauth(AppConfig):
-
-	name = 'oauth'
--- oauth/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from oauth.views import authorize, callback
-
-app_name = 'oauth'
-urlpatterns = [
-	path('<str:app>/authorize', authorize),
-	path('<str:app>/callback', callback)
-]
